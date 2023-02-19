53033518c40edfead2fa7aea035f84603ca59c1d
Chester Woo <chesterwoo@chesters-air.myfiosgateway.com>
2023-02-19 16:08:07 -0500

    Weather URL

--- server/controllers/weather_controller.js
+++ server/controllers/weather_controller.js
@@ -1,7 +1,7 @@
 const fetch = require("node-fetch");
 
 getWeather = async (req, res) => {
-    await fetch(`${process.env.REACT_APP_API_URL}/weather/?lat=40.589409&lon=-73.665939&units=metric&APPID=${process.env.REACT_APP_API_KEY}`)
+    await fetch(`https://api.openweathermap.org/data/2.5/weather/?lat=40.589409&lon=-73.665939&units=metric&APPID=ec7db399a8c578bb32c23f8d8173ff6e`)
     .then(res => res.json())  
     .then(result => {
         console.log(result);
