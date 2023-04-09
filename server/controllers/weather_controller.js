ee64fa96e59c0137ac4a6f853c43636b9b019c83
Chester Woo <chesterwoo@chesters-air.myfiosgateway.com>
2023-04-09 19:56:22 -0400

    Test

--- server/controllers/weather_controller.js
+++ server/controllers/weather_controller.js
@@ -1,6 +1,7 @@
 const fetch = require("node-fetch");
 
 getWeather = async (req, res) => {
+    console.log("Getting Weather!");
     await fetch(process.env.APP_WEATHER_API_URL + "/weather/?lat=40.589409&lon=-73.665939&units=metric&APPID=" + process.env.APP_WEATHER_API_KEY)
     .then(res => res.json())  
     .then(result => {
